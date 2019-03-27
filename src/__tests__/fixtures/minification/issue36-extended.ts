eebc404109428b01db3dae88fb9321b8c14404ea
Igor Oleinikov <igor@oleinikov.ru>
2019-03-26 21:27:32 -0700

    Add more tests with broken cases

--- src/__tests__/fixtures/minification/issue36-extended.ts
+++ src/__tests__/fixtures/minification/issue36-extended.ts
@@ -3,3 +3,17 @@ declare const styled: any;
 export const A = styled.div`
   border: ${'solid'} 10px;
 `
+
+styled.div`
+  border: ${'solid'}// comment here
+10px;
+  border: solid// comment here
+10px;
+`
+
+styled.div`
+  border: ${'solid'}/* comment here
+*/10px;
+  border: ${'solid'}/* comment here
+*/ 10px;
+`
