87ac65ef4e6d5d731065883b960b0cb8f74600c3
Igor Oleinikov <igor.oleinikov@groundtruth.com>
2019-08-28 13:56:12 -0700

    Remove yarn

--- .github/workflows/js/dangerfile.ts
+++ .github/workflows/js/dangerfile.ts
@@ -1,9 +1,9 @@
 import { danger, fail } from 'danger';
-import yarn from 'danger-plugin-yarn'
+// import yarn from 'danger-plugin-yarn'
 // import jest from "danger-plugin-jest"
 
 export default async () => {
-  await yarn()
+  // await yarn()
 
   fail('The danger file is not ready yet');
 }
