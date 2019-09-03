b0d57b8e14220114c6ddc6727bac55bef1020863
Igor Oleinikov <igor.oleinikov@groundtruth.com>
2019-09-03 13:41:37 -0700

    FIx

--- .github/workflows/js/dangerfile.ts
+++ .github/workflows/js/dangerfile.ts
@@ -1,13 +1,14 @@
 import { danger, fail } from 'danger';
-import yarn from 'danger-plugin-yarn'
+// import yarn from 'danger-plugin-yarn'
 // import jest from "danger-plugin-jest"
 
 export default async () => {
   if (danger.github.pr) {
     console.log('PR exists', danger.github.pr.html_url);
+    fail('This dangerfile is not ready yet.');
   } else {
     fail('PR does not exist');
   }
 
-  await yarn();
+  // await yarn();
 }
