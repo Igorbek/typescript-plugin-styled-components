06b092e286f1b0ee152f7ad483ad8e4ab37cc8b4
Igor Oleinikov <igor.oleinikov@groundtruth.com>
2019-09-03 13:29:58 -0700

    Add yarn danger check

--- .github/workflows/js/dangerfile.ts
+++ .github/workflows/js/dangerfile.ts
@@ -1,15 +1,13 @@
 import { danger, fail } from 'danger';
-// import yarn from 'danger-plugin-yarn'
+import yarn from 'danger-plugin-yarn'
 // import jest from "danger-plugin-jest"
 
 export default async () => {
-  // await yarn()
-
   if (danger.github.pr) {
     console.log('PR exists', danger.github.pr.html_url);
   } else {
     fail('PR does not exist');
   }
 
-  fail('The danger file is not ready yet');
+  await yarn();
 }
