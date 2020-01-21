613dc0283185f87b6d828f27a84e83b968e7cf45
Igor Oleinikov <igor.oleinikov@groundtruth.com>
2020-01-21 11:23:21 -0800

    Add fixtures and baselines

---
+++ src/__tests__/fixtures/minification/issue142.tsx
@@ -0,0 +1,10 @@
+declare const styled: any;
+
+const Header = styled.div`
+	display: flex;
+\t  align-items: center;
+	justify-content: space-between;
+\t \t font-weight: 600;
+	padding: 0.8em 1.6em;\t \t
+	background: peachpuff;
+`;
